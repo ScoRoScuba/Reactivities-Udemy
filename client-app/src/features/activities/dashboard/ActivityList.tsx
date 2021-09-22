295dca850ca1432f1af8d1ae4b0c41431e650298
ScoRoScuba <scott.roberts@virgin.net>
2021-09-23 07:57:16 +1000

    end of section 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,27 +1,25 @@
 import React, {SyntheticEvent, useState} from "react";
 import { Label,Button, Item, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/layout/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;    
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+import {observer} from 'mobx-react-lite';
 
-export default function ActivityList({activities,selectActivity,deleteActivity,submitting}: Props) {
+export default observer (function ActivityList() {
+
+    const {activityStore} = useStore();
+    const {activitiesByDate, deleteActivity, loading} = activityStore;
 
     const [target, setTarget] = useState('');
 
     function handleActivityDelete(e : SyntheticEvent<HTMLButtonElement>, id : string){
-        setTarget(e.currentTarget.name);;
+        setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
 
     return (
         <Segment>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activitiesByDate.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -31,9 +29,9 @@ export default function ActivityList({activities,selectActivity,deleteActivity,s
                                 <div>{activity.city},{activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>                                
-                                <Button onClick={()=>selectActivity(activity.id)} floated='right' content='View' color='blue'/>
+                                <Button onClick={()=>activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue'/>
                                 <Button name={activity.id}
-                                        loading={submitting && target === activity.id} 
+                                        loading={loading && target === activity.id} 
                                         onClick={(e)=>handleActivityDelete(e,activity.id)} 
                                         floated='right' 
                                         content='Delete' color='red'/>                                
@@ -45,4 +43,4 @@ export default function ActivityList({activities,selectActivity,deleteActivity,s
             </Item.Group>
         </Segment>
     )
-}
+})
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,13 @@
 import React, {useState,ChangeEvent} from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/layout/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props{
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    submitting: boolean;
-}
+import {observer} from 'mobx-react-lite';
 
-export default function ActivityForm({activity: selectedActivity,closeForm, createOrEdit, submitting}:Props) {
+export default observer ( function ActivityForm() {
+
+    const {activityStore} = useStore();
+    const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
 
     const initialState = selectedActivity ?? {
         id:'',
@@ -24,7 +22,7 @@ export default function ActivityForm({activity: selectedActivity,closeForm, crea
     const [activity, setActivity] = useState(initialState);
 
     function handleSubmit() {
-        createOrEdit(activity);
+        activity.id ? updateActivity(activity): createActivity(activity);
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement|HTMLTextAreaElement>){
@@ -42,9 +40,9 @@ export default function ActivityForm({activity: selectedActivity,closeForm, crea
                 <Form.Input placeholders='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholders='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
 
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
+                <Button loading={loading} floated='right' positive type='submit' content='Submit'/>
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
     )
-}
+})
